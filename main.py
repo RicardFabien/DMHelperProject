7abba4e812e3ff2c78c65e902c74431938e8315f
RicardFabien <ricard.fabienpro92@yahoo.fr>
2021-06-07 16:33:01 +0200

    added message answering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -12,5 +13,17 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    print("Reçu " + message.content)
+
+    if message.content == 'test':
+        response = "test"
+        await message.channel.send(response)
 
 client.run(TOKEN)
