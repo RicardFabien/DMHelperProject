8b3b74327241b5a4b29c2040f918c989128f7871
RicardFabien <ricard.fabienpro92@yahoo.fr>
2021-06-09 16:35:59 +0200

    Changes by homme

--- CharacterPortfolio.py
+++ CharacterPortfolio.py
@@ -1,12 +1,4 @@
-
-
-class Character:
-    def __init__(self, name):
-        self.name = name
-    name = ""
-    stats = dict()
-    attributes = []
-
+import Character
 
 class CharacterPortfolio:
     characters = []
@@ -15,15 +7,18 @@ class CharacterPortfolio:
         new_char = Character(name)
         self.characters.append(new_char)
 
+
     def find_by_name(self, name):
-        for character in self.characters :
-            if character.name == name :
+        for character in self.characters:
+            if character.name == name:
                 return character
         return False
 
     def remove_character(self, name):
         self.characters.remove(self.find_by_name(name))
 
+    def modif_character(self, name):
+        pass                                                                                                                                                                                          d        f
 
 
 
